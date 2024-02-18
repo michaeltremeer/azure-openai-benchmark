3468d2ca97ea69c4606949b4f8ffacde5a4f80a5
Michal@MacBook <michal.marusan@gmail.com>
2024-02-18 21:14:20 +0100

    more parameters

--- gui.py
+++ gui.py
@@ -16,6 +16,11 @@ from dotenv import load_dotenv
 if not load_dotenv("../.env"):
     load_dotenv(".env")
 
+
+#################################################################################
+# Context and configuration
+#################################################################################
+
 if "info" not in st.session_state:
     st.session_state.info = None
 
@@ -34,28 +39,49 @@ if "rate" not in st.session_state:
 if "shape_profile" not in st.session_state:
     st.session_state.shape_profile = "context"
 
-AZURE_API_KEY = os.environ["OPENAI_API_KEY"]
-AZURE_API_URL = os.environ['OPENAI_API_URL']
+if "context_tokens" not in st.session_state:
+    st.session_state.context_tokens = 2000
+if "max_tokens" not in st.session_state:
+    st.session_state.max_tokens = 200
 
-if AZURE_API_KEY == "" or AZURE_API_URL == "":
-    st.error("Please set the OPENAI_API_KEY and OPENAI_API_URL environment variables")
+if "clients" not in st.session_state:
+    st.session_state.clients = 20
+
+if "aggregation_window" not in st.session_state:
+    st.session_state.aggregation_window = 120
+
+if "batch_run" not in st.session_state:
+    st.session_state.batch_run = False
+
+if "AZURE_API_KEY" not in st.session_state:
+    st.session_state.AZURE_API_KEY = ""
+
+if "AZURE_API_URL" not in st.session_state:
+    st.session_state.AZURE_API_URL = ""
+
+if "stop_on_timeout" not in st.session_state:
+    st.session_state.stop_on_timeout = False
+
+if "stop_on_timeout_value" not in st.session_state:
+    st.session_state.stop_on_timeout_value = 120
+
+if "stop_on_requests" not in st.session_state:
+    st.session_state.stop_on_requests = False
+
+if "stop_on_requests_value" not in st.session_state:
+    st.session_state.stop_on_requests_value = 100
 
 
-# Example - Run a single batch of the following two configuration for 120 seconds each, making sure to warm up the PTU-M endpoint prior to each run:
+st.session_state.AZURE_API_KEY = os.environ["OPENAI_API_KEY"]
+st.session_state.AZURE_API_URL = os.environ['OPENAI_API_URL']
 
-# context_tokens=500, max_tokens=100, rate=20
-# context_tokens=3500, max_tokens=300, rate=7.5
-# python -m benchmark.contrib.batch_runner https://gbb-ea-openai-swedencentral-01.openai.azure.com/ 
-#     --deployment gpt-4-1106-ptu 
-#     --token-rate-workload-list 500-100-20,3500-300-7.5 
-#     --duration 130 
-#     --aggregation-window 120 
-#     --log-save-dir logs/ 
-#     --start-ptum-runs-at-full-utilization true
-#
-# 
+# if AZURE_API_KEY == "" or AZURE_API_URL == "":
+#     st.error("Please set the OPENAI_API_KEY and OPENAI_API_URL environment variables")
+
+
 #################################################################################
-# App elements
+# Pager setup & functions
+#################################################################################
 
 st.set_page_config(layout="wide")
 st.title("Azure OpenAI PTU Benchmark")
@@ -66,65 +92,126 @@ def format_num(x):
     else:
         return int(x)
 
-# Use the get method since the keys won't be in session_state
-# on the first script run
-
-with st.expander("Settings"):
+#################################################################################
+# Settings
+#################################################################################
+with st.expander("Settings", expanded=True):
     st.caption("To run the benchmark, make sure to set the OPENAI_API_KEY and OPENAI_API_URL environment variables")
+    if st.session_state.AZURE_API_KEY == "" or st.session_state.AZURE_API_URL == "":
+        with st.container(border=True):
+            st.session_state.AZURE_API_URL = st.text_input("Azure API URL", "https://<YOUR_ENDPOINT>.openai.azure.com/")
+            st.session_state.AZURE_API_KEY = st.text_input("Azure API Key", st.session_state.AZURE_API_KEY, type="password")
 
     st.session_state.model = st.selectbox("Select a model", [  "gpt-4-turbo", "gpt-4","gpt-35-turbo"])
     st.session_state.rate = st.slider("Rate", 1, 300, 5, 1)
-    st.session_state.shape_profile = st.selectbox("Shape", ["context", "balanced", "generation"])
+    st.session_state.aggregation_window = st.text_input("Aggregation window (in seconds)", "120")
+    st.session_state.clients = st.slider("Clients", 1, 100, 20, 5)
+    st.session_state.shape_profile = st.selectbox("Shape", ["context", "balanced", "generation", "custom"])
+
+    if st.session_state.shape_profile == "custom":
+        
+        st.session_state.context_tokens = st.slider("Context tokens", 100, 16000, 2000, 100)
+        st.session_state.max_tokens = st.slider("Max tokens", 1, 1000, 200, 10)
 
     if st.session_state.shape_profile == "balanced":
-        context_tokens = 500
-        max_tokens = 500
+        st.session_state.context_tokens = 500
+        st.session_state.max_tokens = 500
     elif st.session_state.shape_profile == "context":
-        context_tokens = 2000
-        max_tokens = 200
+        st.session_state.context_tokens = 2000
+        st.session_state.max_tokens = 200
     elif st.session_state.shape_profile == "generation":
-        context_tokens = 500
-        max_tokens = 1000
+        st.session_state.context_tokens = 500
+        st.session_state.max_tokens = 1000
 
-    load_max = max(st.session_state.rate - 7,1) * (context_tokens + max_tokens)
-    load_min = max(st.session_state.rate - 20,1) * (context_tokens + max_tokens)
+    # stopping policy
+    st.caption("The benchmark will run until stopped manually")
+    if st.toggle('Stop on timeout', value=False):
+        st.session_state.stop_on_timeout = True
+        st.session_state.stop_on_timeout_value = st.slider("Duration [s]", 60, 3600, 120, 60)
+    if st.toggle('Stop on request count', value=False):
+        st.session_state.stop_on_requests = True
+        st.session_state.stop_on_requests_value = st.slider("No of requests", 100, 10000, 100, 100)
+
+    load_max = max(st.session_state.rate - 7,1) * (st.session_state.context_tokens + st.session_state.max_tokens)
+    load_min = max(st.session_state.rate - 20,1) * (st.session_state.context_tokens + st.session_state.max_tokens)
     formatted_load_min = f"{load_min//1000}K"
     formatted_load_max = f"{load_max//1000}K"
     st.caption(f"Based on your settings, the benchmark generated load will range from ```{formatted_load_min}``` to ```{formatted_load_max}```\n\n---")
 
-if st.session_state.get('run'):
-    st.session_state.running = True
-    
-    my_env = os.environ.copy()
-    my_env["OPENAI_API_KEY"] = AZURE_API_KEY
-    
-    output_folder = "test_results"
-    command = f"python -m benchmark.bench load --deployment {st.session_state.model} --rate {st.session_state.rate} --shape-profile {st.session_state.shape_profile} --clients 80 --output-format jsonl --log-save-dir {output_folder} {AZURE_API_URL}"
-    # print (command)
+#################################################################################
+# Run benchmark area
+#################################################################################
+with st.container(border=True):
+    st.caption(f"Run the benchmark with selected parameters: Model: {st.session_state.model} | Rate: {st.session_state.rate} | Clients: {st.session_state.clients} | Aggregation window: {st.session_state.aggregation_window} | Shape: {st.session_state.shape_profile} | Context tokens: {st.session_state.context_tokens} | Max tokens: {st.session_state.max_tokens}")
+    if st.session_state.get('run'):
 
-    st.session_state.proc = subprocess.Popen(command.split(), env=my_env)
-    time.sleep(3)
-    
-    poll = st.session_state.proc.poll()
-    if poll is None:
-        st.session_state.info = st.info(f"Running benchmark as {st.session_state.proc.pid}")
-    else:
-        st.session_state.info = st.error(f"Error running benchmark: {poll}")
-    
-if st.session_state.get('stop'):
-    st.session_state.running = False
+        if st.session_state.AZURE_API_KEY == "" or st.session_state.AZURE_API_URL == "":
+            st.error("Please set the OPENAI_API_KEY and OPENAI_API_URL environment variables or use settings above to set them.")
+        else:
+            st.session_state.running = True
+            my_env = os.environ.copy()
+            my_env["OPENAI_API_KEY"] = st.session_state.AZURE_API_KEY
 
-    parent_pid = st.session_state.proc.pid   # my example
-    parent = psutil.Process(parent_pid)
-    for child in parent.children(recursive=True):  # or parent.children() for recursive=False
-        child.kill()
-    parent.kill()
+            # Example - Run a single batch of the following two configuration for 120 seconds each, making sure to warm up the PTU-M endpoint prior to each run:
 
-col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
-with col2:
-    st.button('RUN', key='run', disabled=st.session_state.running)
-with col3:
-    st.button('STOP', key='stop', type="primary", disabled=not st.session_state.running)
+            # context_tokens=500, max_tokens=100, rate=20
+            # context_tokens=3500, max_tokens=300, rate=7.5
+            # python -m benchmark.contrib.batch_runner https://gbb-ea-openai-swedencentral-01.openai.azure.com/ 
+            #     --deployment gpt-4-1106-ptu 
+            #     --token-rate-workload-list 500-100-20,3500-300-7.5 
+            #     --duration 130 
+            #     --aggregation-window 120 
+            #     --log-save-dir logs/ 
+            #     --start-ptum-runs-at-full-utilization true
+            #
+            # 
+
+            output_folder = "test_results"
+            
+            if st.session_state.batch_run:
+                st.write('Batch comming soon... running normally')
+            
+            command  = f"python -m benchmark.bench load --deployment {st.session_state.model} --rate {st.session_state.rate} --shape-profile {st.session_state.shape_profile} --clients {st.session_state.clients} --output-format jsonl --log-save-dir {output_folder} "
+            
+            if st.session_state.shape_profile == "custom":
+                command += f"--context-tokens {st.session_state.context_tokens} --max-tokens {st.session_state.max_tokens} "
+            
+            if st.session_state.stop_on_timeout:
+                command += f" --duration {st.session_state.stop_on_timeout_value} "
+            elif st.session_state.stop_on_requests:
+                command += f" --requests {st.session_state.stop_on_requests_value} "
+            
+            
+            command += f" {st.session_state.AZURE_API_URL}"
+            # print (command)
+
+            st.session_state.proc = subprocess.Popen(command.split(), env=my_env)
+            
+            poll = st.session_state.proc.poll()
+            if poll is None:
+                # st.session_state.info = st.info(f"Running benchmark as {st.session_state.proc.pid}")
+                pass
+            else:
+                st.session_state.info = st.error(f"Error running benchmark: {poll}")
+        
+    if st.session_state.get('stop'):
+        st.session_state.running = False
+        
+        parent_pid = st.session_state.proc.pid   # my example
+        parent = psutil.Process(parent_pid)
+        for child in parent.children(recursive=True):  # or parent.children() for recursive=False
+            child.kill()
+        parent.kill()
+
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+    with col1:
+        st.session_state.batch_run = st.toggle('Batch runner', disabled=st.session_state.running)
+
+       
+    with col2:
+        st.button('START', key='run', disabled=st.session_state.running)
+    with col3:
+        st.button('STOP', key='stop', type="primary", disabled=not st.session_state.running)
 
 
 st.title("Benchmark Results:")
@@ -133,7 +220,7 @@ if st.button("Refresh"):
     pass
 
 if st.session_state.running:
-    st.session_state.info = st.warning(f"Running benchmark as {st.session_state.proc.pid} (params: {st.session_state.model}, {st.session_state.rate}, {st.session_state.shape_profile})")
+    st.session_state.info = st.info(f"Running benchmark as {st.session_state.proc.pid} (params: {st.session_state.model}, {st.session_state.rate}, {st.session_state.shape_profile})")
 else:
     st.session_state.info = None
 
@@ -143,6 +230,8 @@ TESTS_RESULTS_FILE = "o.jsonl"
 if not os.path.exists(TESTS_RESULTS_FOLDER):
     os.makedirs(TESTS_RESULTS_FOLDER)
 
+
+   
 # list all CSV files in TESTS_RESULTS_FOLDER
 files = [f for f in os.listdir(TESTS_RESULTS_FOLDER) if f.endswith('.log')]
 
